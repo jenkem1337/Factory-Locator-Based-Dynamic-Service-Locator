a9ab3eb794931aea4d5156c3cbd460b6dc7f1a2b
Can <78204287+jenkem1337@users.noreply.github.com>
2022-02-01 00:16:28 +0300

    c0mit

--- Services/ABCSingletonService.ts
+++ Services/ABCSingletonService.ts
@@ -1,5 +1,9 @@
 import Singleton from '../Singleton';
 export default class ABCService extends Singleton{
+    public constructor(){
+        super()
+    }
+
     public static getInstance(){
         return this.createSingletonInstance(this)
     }
